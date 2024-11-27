c86ba1d474a5369c18ce7f4b592a4747e3b1aa79
Jorge Ruiz <joyares@gmail.com>
2024-11-26 20:01:04 -0600

    Se agregan funciones de busqueda y filtrado en el proyecto 04

--- 04-buscador/src/views/search/components/SearchBox/index.js
+++ 04-buscador/src/views/search/components/SearchBox/index.js
@@ -1,22 +1,32 @@
 import { useState } from "react";
+import "./style.css";
 
-const SearchBox = () => {
+const SearchBox = ({onSearch, onClose}) => {
     const [searchText,setSearchText] = useState("");
 
     const handleSearchText = (event) => {
         setSearchText(event.target.value);
-    }
+    };
+
+    const handleSearchClick = () => {
+        setSearchText("");
+        onClose();
+    };
 
     return(
-        <div>
-            <div>
-                Buscador de personal
+        <div className="search-box">
+            <h2 className="search-box-titulo">Buscador de personal</h2>
+            <div className="search-box-buttons">
                 <label>
-                    <input value={searchText} onChange={handleSearchText}></input>
+                    <input 
+                    value={searchText} 
+                    onChange={handleSearchText}
+                    className="search-box-input"
+                    ></input>
                 </label>
-                <button>Buscar</button>
+                <button onClick={ () => onSearch(searchText) }>Buscar</button>
+                <button onClick={ handleSearchClick }>Cerrar</button>
             </div>
-            
         </div>
     );
 }
--- 04-buscador/src/views/search/index.js
+++ 04-buscador/src/views/search/index.js
@@ -1,10 +1,43 @@
 
+import { useState } from "react";
 import SearchBox from "./components/SearchBox/index";
+import "./style.css"
+import data from  "../../data/users.json"
 
 const Search = () => {
+
+    const [isAtTop,setIsAtTop] = useState(false);
+
+    const [usersData,setUsersData] = useState(data);
+
+    const [results,setResults] = useState();
+
+    const handleSearchClick = (searchText) => {
+        if(usersData?.length) {
+            const searchTextMinus = searchText.toLowerCase();
+            const filteredData = usersData.filter((value) => {
+                return (
+                    value.name.toLowerCase().includes(searchTextMinus) ||
+                    value.phone.toLowerCase().includes(searchTextMinus) ||
+                    value.email.toLowerCase().includes(searchTextMinus) ||
+                    value.username.toLowerCase().includes(searchTextMinus)
+                )
+            });
+            setResults(filteredData);
+        }
+        setIsAtTop(true);
+    };
+
+    const handleCloseClick = () => {
+        setIsAtTop(false);
+        setResults([]);
+    };
+
+    
+console.log(results);
     return (
-        <div>
-            <SearchBox />
+        <div className={`search ${isAtTop ? "search--top" : "search--center"}`}>
+            <SearchBox onSearch={handleSearchClick} onClose={handleCloseClick}/>
         </div>
     )    
 }
